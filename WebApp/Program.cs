829d6616f7ea632936824c70b86a04aef5757ed2
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2023-04-15 15:35:46 +0200

    Added login, logout and registration form

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,11 +1,23 @@
 using WebApp.Services;
+using WebApp.Contexts;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ShowcaseService>();
+builder.Services.AddDbContext<IdentityContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("IdentitySql")));
 
+builder.Services.AddIdentity<CustomIdentityUser, IdentityRole>(x =>
+{
+    x.SignIn.RequireConfirmedAccount = false;
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequiredLength = 8;
+
+}).AddEntityFrameworkStores<IdentityContext>();
 
 
 
---
+++ WebApp/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
